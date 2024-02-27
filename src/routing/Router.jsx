7baa98e07c42b2cf3b6f66d9dc98ae1fd8f25bb6
xlusivecharles <charlesblaze193@gmail.com>
2024-02-27 15:00:41 +0100

    Had to clone and raise a new PR again, hopefully this works out

--- src/routing/Router.jsx
+++ src/routing/Router.jsx
@@ -5,15 +5,17 @@ import Register from "../pages/auth/Register";
 import AdminLogin from "../pages/auth/AdminLogin";
 import Index from "../pages/Index";
 import AdminSignUp from "./../pages/auth/AdminSignUp";
+import ResetPassword from "../pages/auth/ResetPassword";
 
 export default function Router() {
-	return (
-		<Routes>
-			<Route path="/" element={<Index />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/register" element={<Register />} />
-			<Route path="/admin-login" element={<AdminLogin />} />
-			<Route path="/admin-signup" element={<AdminSignUp />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/admin-login" element={<AdminLogin />} />
+      <Route path="/admin-signup" element={<AdminSignUp />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+    </Routes>
+  );
 }
