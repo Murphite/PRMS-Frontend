2f10e94ebd54419b1c0b03be246f1e944ee28f3b
Ebeleokolo <ebelelynda.okolo@gmail.com>
2024-02-29 02:55:00 +0100

    Consume AdminLogin Endpoint

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,29 +1,42 @@
 import { axiosApi } from "./apiService";
 
 export const login = async (email, password) => {
-	try {
-		const res = await axiosApi.post(`/auth/login`, {
-			email,
-			password,
-		});
+    try {
+        const res = await axiosApi.post(`/auth/login`, {
+            email,
+            password,
+        });
 
-		return res.data;
-	} catch (error) {
-		return error;
-	}
+        return res.data;
+    } catch (error) {
+        return error;
+    }
 };
 
 export const signUp = async (firstName, lastName, email, password) => {
-	try {
-		const res = await axiosApi.post(`/auth/register`, {
-			firstName,
-			lastName,
-			email,
-			password,
-		});
+    try {
+        const res = await axiosApi.post(`/auth/register`, {
+            firstName,
+            lastName,
+            email,
+            password,
+        });
 
-		return res.data;
-	} catch (error) {
-		return error;
-	}
+        return res.data;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const adminLogin = async (email, password) => {
+    try {
+        const res = await axiosApi.post(`/auth/admin-login`, {
+            email,
+            password,
+        });
+
+        return res.data;
+    } catch (error) {
+        return error;
+    }
 };
