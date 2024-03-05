7445b19e86b7574362a12fe7ea967aa7943ca146
Ebeleokolo <ebelelynda.okolo@gmail.com>
2024-03-05 03:45:01 +0100

    HCS-29-adminLogin-Endpoint updated

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { axiosApi } from "./apiService";
+import { useNavigate } from "react-router-dom";
 
 export const login = async (email, password) => {
     try {
@@ -28,15 +30,58 @@ export const signUp = async (firstName, lastName, email, password) => {
     }
 };
 
-export const adminLogin = async (email, password) => {
-    try {
-        const res = await axiosApi.post(`/auth/admin-login`, {
-            email,
-            password,
-        });
+export default function adminLogin() {
+    const { setAccessToken, setAuthUserData } = userContext.AppContext;
+    const { data, setData } = useState({
+        email: "",
+        password: "",
+        remember: false,
+    });
+    const [errors, setErrors] = useState({
+        email: "",
+    });
+    const navigate = useNavigate();
 
-        return res.data;
-    } catch (error) {
-        return error;
-    }
-};
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setData({ ...data, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await adminLogin(data.email, data.password);
+
+        if (res.error?.length > 0) {
+            console.log(res.errors);
+            setErrors({ ...errors, email: res.errors[0].message });
+            return;
+        }
+
+        setAccessToken(res.data.accessToken);
+        setAuthUserData(res.data.userInfo);
+        navigate("/dashboard");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input
+                    type="text"
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
+                <button type="submit">Login</button>
+                {errors.email && <div>{errors.email}</div>}
+            </div>
+        </form>
+    );
+}
