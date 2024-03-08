f158d39519c67efd33d5e13602c053cbfac249f2
Babatunde Mion <Fawazboluwatife7@gmail.com>
2024-03-08 16:37:50 +0100

    wip

---
+++ src/components/CreatePatientForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreatePatientForm = () => {
+  return (
+    <form>CreatePatientForm</form>
+  )
+}
+
+export default CreatePatientForm
---
+++ src/pages/dashboard/CreatePatient.jsx
@@ -0,0 +1,10 @@
+import CreatePatientForm from "../../components/CreatePatientForm";
+import DashLayout from "../../layouts/DashLayout";
+
+const CreatePatient = () => {
+    return <DashLayout>
+        <CreatePatientForm />
+    </DashLayout>;
+};
+
+export default CreatePatient;
