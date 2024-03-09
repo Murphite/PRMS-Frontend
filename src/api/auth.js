cd76f4fd47e2e2c54a90479bb4313c9654b8c2f6
Ebeleokolo <ebelelynda.okolo@gmail.com>
2024-03-09 03:14:06 +0100

    Consume adminlogin endpoint changes implemented

--- src/api/auth.js
+++ src/api/auth.js
@@ -30,58 +30,14 @@ export const signUp = async (firstName, lastName, email, password) => {
     }
 };
 
-export default function adminLogin() {
-    const { setAccessToken, setAuthUserData } = userContext.AppContext;
-    const { data, setData } = useState({
-        email: "",
-        password: "",
-        remember: false,
-    });
-    const [errors, setErrors] = useState({
-        email: "",
-    });
-    const navigate = useNavigate();
-
-    const handleChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-
-        setData({ ...data, [name]: value });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const res = await adminLogin(data.email, data.password);
-
-        if (res.error?.length > 0) {
-            console.log(res.errors);
-            setErrors({ ...errors, email: res.errors[0].message });
-            return;
-        }
-
-        setAccessToken(res.data.accessToken);
-        setAuthUserData(res.data.userInfo);
-        navigate("/dashboard");
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <input
-                    type="text"
-                    name="email"
-                    value={data.email}
-                    onChange={handleChange}
-                />
-                <input
-                    type="password"
-                    name="password"
-                    value={data.password}
-                    onChange={handleChange}
-                />
-                <button type="submit">Login</button>
-                {errors.email && <div>{errors.email}</div>}
-            </div>
-        </form>
-    );
-}
+export const adminLogin = async (email, password) => {
+    try {
+        const res = await axiosApi.post(`/auth/admin-login`, {
+            email,
+            password,
+        });
+        return res.data;
+    } catch (error) {
+        return error;
+    }
+};
