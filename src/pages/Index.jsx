ffe952ecbbf8faa56afed5ab2a0372f689db378a
Favour Olanipekun <olamidefavour619@outlook.com>
2024-03-14 13:07:31 +0100

    Changes made to the Landing Page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,17 @@
+import HeroSection from "../components/HeroSection";
 import JoinUsSection from "../components/JoinUsSection";
+import KeyFeatures from "../components/KeyFeatures";
+import SubscribeForm from "../components/SubscribeForm";
 import WhyUsSection from "../components/WhyUsSection";
 
 const Index = () => {
     return (
         <div>
+            <HeroSection />
+            <KeyFeatures />
             <WhyUsSection />
             <JoinUsSection />
+            <SubscribeForm />
         </div>
     );
 };
