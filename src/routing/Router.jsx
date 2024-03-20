a2a9429209efd08c040fa9bba2ee9151a8515128
Babatunde Mion <Fawazboluwatife7@gmail.com>
2024-03-20 16:54:50 +0100

    necessary changes

--- src/routing/Router.jsx
+++ src/routing/Router.jsx
@@ -11,7 +11,7 @@ import EmailVerification from "../pages/auth/EmailVerification";
 import ResetPassword from "../pages/auth/ResetPassword";
 import RegularSignUp from "../pages/auth/RegularSignUp";
 import PatientsList from "../pages/dashboard/PatientsList";
-// import EmailVerificationStatus from "../pages/auth/EmailVerificationStatus";
+import EmailVerificationStatus from "../pages/auth/EmailVerificationStatus";
 import RegularLogin from "../pages/auth/RegularLogin";
 import CreatePatient from "../pages/dashboard/CreatePatient";
 import CreateAppointment from "../pages/dashboard/CreateAppointment";
@@ -22,7 +22,7 @@ export default function Router() {
     return (
         <Routes>
             <Route path="/" element={<Index />} />
-            {/* <Route path="/login" element={<Login />} /> */}
+            <Route path="/login" element={<Login />} /> 
             <Route path="/register" element={<Register />} />
             <Route path="/admin-login" element={<AdminLogin />} />
             <Route path="/admin-signup" element={<AdminSignUp />} />
@@ -31,10 +31,10 @@ export default function Router() {
             <Route path="/email-verification" element={<EmailVerification />} />
             <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/signup" element={<RegularSignUp />} />
-            {/* <Route
+             <Route
                 path="/confirm-email"
                 element={<EmailVerificationStatus />}
-            /> */}
+            /> 
             <Route path="/login" element={<RegularLogin />} />
 
             <Route path="/dashboard/patients" element={<PatientsList />} />
