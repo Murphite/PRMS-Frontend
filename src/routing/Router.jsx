f5ef16bafe01f0428193a950b89a46f64df5a1a7
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2024-03-25 01:51:38 +0100

    First commit

--- src/routing/Router.jsx
+++ src/routing/Router.jsx
@@ -17,6 +17,7 @@ import CreatePatient from "../pages/dashboard/CreatePatient";
 import CreateAppointment from "../pages/dashboard/CreateAppointment";
 import ViewPatient from "../pages/admin/ViewPatient";
 import DashBoardPage from "../pages/dashboard/dashBoardPage";
+import PrescriptionOverviewTableAdmin from "../components/PrescriptionOverviewTableAdmin";
 
 export default function Router() {
     return (
